258258bd73e4b7d83345208e6e845f98e066541e
L1nSn0w <l1nsn0w@qq.com>
2025-10-07 17:59:29 +0800

    Add logo

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,5 +1,6 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
+
 /**
  * Shared layout configurations
  *
@@ -12,15 +13,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+          <img src='/logo.svg' alt='Ech0' className='inline h-6 mr-2' />
+          Ech0
         </>
       ),
     },
