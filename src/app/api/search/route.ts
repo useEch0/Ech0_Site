ec37d3a9e68e2aa2198f7854d43f19e242811baf
minc-nice-100 <100515973+minc-nice-100@users.noreply.github.com>
2025-10-18 16:37:25 +0800

    refactor: remove unused search and og routes

--- src/app/api/search/route.ts
+++
@@ -1,8 +0,0 @@
-// TODO 支持中文
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
-
-export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/docs/orama-js/supported-languages
-  language: 'english',
-});
